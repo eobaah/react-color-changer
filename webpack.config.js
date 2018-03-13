21dcf9cfa4c3fd8ec6df608694f6c9720702ae7d
Gulmira Berdalieva <gulmira2410@gmail.com>
2018-03-13 16:08:04 -0700

    change Readme.md

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./config/webpack.common.config');
 
-module.exports = (env) => {
-    
-    const determineAddons = (addons) => {
-        return [...[addons]]
-            .filter(addon => Boolean(addon))
-            .map(addon => require(`./config/addons/webpack.${addon}.js`));
-    };
+module.exports = env => {
+  const determineAddons = addons => {
+    return [...[addons]].filter(addon => Boolean(addon)).map(addon => require(`./config/addons/webpack.${addon}.js`));
+  };
 
-    const envConfig = require(`./config/webpack.${env.env}.config`);
+  const envConfig = require(`./config/webpack.${env.env}.config`);
 
-    return webpackMerge(commonConfig, envConfig, ...determineAddons(env.addons));
+  return webpackMerge(commonConfig, envConfig, ...determineAddons(env.addons));
 };
